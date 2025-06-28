a8251dac9c8da597415833557a911043b432c7ce
eorthus <oakenshielddis@gmail.com>
2025-06-28 18:28:23 +0300

    fix: front security headers add

--- frontend/server/middleware/security.ts
+++ frontend/server/middleware/security.ts
@@ -1,30 +1,30 @@
 export default defineEventHandler((event) => {
-//     const headers = event.node.res.getHeaders();
+    const headers = event.node.res.getHeaders();
     
-//     // Защита от XSS
-//     event.node.res.setHeader('X-XSS-Protection', '1; mode=block');
+    // Защита от XSS
+    event.node.res.setHeader('X-XSS-Protection', '1; mode=block');
     
-//     // Предотвращение кликджекинга
-//     event.node.res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+    // Предотвращение кликджекинга
+    event.node.res.setHeader('X-Frame-Options', 'SAMEORIGIN');
     
-//     // Запрет браузеру угадывать тип контента
-//     event.node.res.setHeader('X-Content-Type-Options', 'nosniff');
+    // Запрет браузеру угадывать тип контента
+    event.node.res.setHeader('X-Content-Type-Options', 'nosniff');
     
-//     // Базовая CSP для защиты от XSS
-//     event.node.res.setHeader(
-//       'Content-Security-Policy',
-//       "default-src 'self'; script-src 'self' https://cdnjs.cloudflare.com; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://s3.stroi-news.ru; connect-src 'self'"
-//     );
+    // Базовая CSP для защиты от XSS
+    event.node.res.setHeader(
+      'Content-Security-Policy',
+      "default-src 'self'; script-src 'self' https://cdnjs.cloudflare.com; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://s3.stroi-news.ru; connect-src 'self'"
+    );
     
-//     // Разрешаем загрузку только по HTTPS (если сайт работает по HTTPS)
-//     event.node.res.setHeader(
-//       'Strict-Transport-Security',
-//       'max-age=31536000; includeSubDomains'
-//     );
+    // Разрешаем загрузку только по HTTPS (если сайт работает по HTTPS)
+    event.node.res.setHeader(
+      'Strict-Transport-Security',
+      'max-age=31536000; includeSubDomains'
+    );
     
-//     // Параметры безопасности для куки
-//     event.node.res.setHeader(
-//       'Set-Cookie',
-//       'Path=/; HttpOnly; Secure; SameSite=Strict'
-//     );
+    // Параметры безопасности для куки
+    event.node.res.setHeader(
+      'Set-Cookie',
+      'Path=/; HttpOnly; Secure; SameSite=Strict'
+    );
   });
